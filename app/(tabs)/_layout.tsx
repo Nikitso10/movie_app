bd66cfb36115e1877a16abb2d09dc6b40a9b593d
Niki Tsolaki <nikitsolaki10@gmail.com>
2025-08-12 16:13:22 +0300

    tabs layout init commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,83 @@
-import { Tabs } from 'expo-router';
+import {View, Text, ImageBackground, Image} from 'react-native';
 import React from 'react';
-import { Platform } from 'react-native';
+import {Tabs} from "expo-router";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { images } from "@/constants/images";
+import {icons} from "@/constants/icons";
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+const TabIcon = ({ focused, icon, title }) => {
+    return (
+        <ImageBackground
+            source = {images.highlight}
+            className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        >
+            <Image source = {icon} tintColor="#151312" className="size-5"
+            />
+            <Text className="text-secondary text-base font-semibold">{title}</Text>
+        </ImageBackground>
+    )
 }
+
+const _Layout = () => {
+    return (
+        <Tabs>
+            <Tabs.Screen
+            name="index"
+            options={{
+                headerShown: false,
+                title: 'Home',
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon
+                        focused={focused}
+                        icon={icons.home}
+                        title="Home"
+                    />
+                )
+            }}
+            />
+            <Tabs.Screen
+                name="search"
+                options={{
+                    headerShown: false,
+                    title: 'Search',
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.search}
+                            title="Search"
+                        />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="saved"
+                options={{
+                    headerShown: false,
+                    title: 'Saved',
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.save}
+                            title="Saved"
+                        />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    headerShown: false,
+                    title: 'Profile',
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.person}
+                            title="Profile"
+                        />
+                    )
+                }}
+            />
+        </Tabs>
+    )
+}
+export default _Layout
